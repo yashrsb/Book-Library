9d9612cc37a311f21dbd0f9b8049b871af181248
YashRaj-Git <yash.bisht@satschel.com>
2026-01-04 00:44:06 +0530

    add: isVerified check for all api requests

--- src/services/auth/dependencies.py
+++ src/services/auth/dependencies.py
@@ -13,7 +13,8 @@ from src.errors import (
     InvalidToken,
     RefreshTokenRequired,
     AccessTokenRequired,
-    InsufficientPermission
+    InsufficientPermission,
+    AccountNotVerified
 )
 
 user_service = UserService()
@@ -64,6 +65,8 @@ class RoleChecker:
     def __init__(self, allowed_roles: List[str]) -> None:
         self.allowed_roles = allowed_roles
     def __call__(self, current_user: User = Depends(get_current_user)) -> any:
+        if not current_user.is_verified:
+            raise AccountNotVerified()
         if current_user.role in self.allowed_roles:
             return True
         raise InsufficientPermission()
