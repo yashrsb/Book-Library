c5839550b1798f11fdef13d8f378ac048e9abd01
YashRaj-Git <yash.bisht@satschel.com>
2026-01-30 12:33:15 +0530

    fix: config import path for celery

--- src/celery_tasks.py
+++ src/celery_tasks.py
@@ -4,7 +4,7 @@ from asgiref.sync import async_to_sync
 
 c_app = Celery()
 
-c_app.config_from_object("src.config")
+c_app.config_from_object("config")
 
 @c_app.task()
 def send_mail(recipients: list[str], subject: str, body: str):
